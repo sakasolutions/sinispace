16baa98fff6cd31844ce81a94aed1bcb647767e0
Sinan Sakacilar <sinansakacilar@Air-von-Sinan.fritz.box>
2025-10-14 22:52:33 +0200

    fix: Remove stray characters from firebase-admin

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -2,8 +2,6 @@ import admin from 'firebase-admin';
 
 if (!admin.apps.length) {
   try {
-    // Diese Zeile ist der Schlüssel zum Sieg!
-    // Sie packt den sicheren Schlüssel aus, bevor er verwendet wird.
     const serviceAccountString = Buffer.from(
       process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string,
       'base64'
@@ -23,4 +21,3 @@ const adminDb = admin.firestore();
 const adminAuth = admin.auth();
 
 export { adminDb, adminAuth };
------
